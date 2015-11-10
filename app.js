be9a253463032ab312428cb1db93bad4a1c3bfa9
nguyemic <nguyemic@uw.edu>
2015-11-10 00:36:40 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
+//intialize variables
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=artist&query='
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
 var myApp = angular.module('myApp', [])
 
+//called the overall controller method
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
+  //method that grabbed the track array off of spotify's API and 
+  //then stored it as data that can be referenceed in html later
   $scope.getSongs = function() {
-    link = baseUrl + $scope.album;
-    $http.get(baseUrl + $scope.album).success(function(response){
-      var id = response.artists.items[0].id
-      var albumURL = 'https://api.spotify.com/v1/artists/' + id + '/albums'
-
-      $http.get(albumURL).success(function(response){
-        data = $scope.albums = response.items;
-        
-        console.log(data);
-        console.log(albumURL);
-
-      })
+    $http.get(baseUrl + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items
+      console.log(data)
     })
   }
 
+
+  //play function
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
